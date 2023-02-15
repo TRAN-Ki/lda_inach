d80d6302e82771d54128406408f7a40013c28bbd
francois <totofrancois12@gmail.com>
2023-02-15 15:10:49 +0100

    Fix : purge de du projet et installation de react router

---
+++ src/Component/Navemenue.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {BrowserRouter} from "react-router-dom";
+
+function Navemenue(props) {
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default Navemenue;
